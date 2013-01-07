646229046d14ba8c6c0a60e203d9b4674a8eaf90
mdino <dino.musliu@gmail.com>
2013-01-07 12:49:49 +0100

    Update polja.h

--- polja.h
+++ polja.h
@@ -1,4 +1,3 @@
-
 struct tljudi {
     int xi,yi,prioritet,transakcija,godina;
     char ime[40],prezime[40],devizni_valuta[40];
@@ -36,14 +35,3 @@ void DeQueueQ(telement*element) {
     element->front=glava->sljedeci;
     delete glava;
 }
-//FrontQ(Q) - Funkcija koja vraca vrijednost
-//elementa koji se trenutno nalazi na celu reda.
-//EnQueueQ(x,Q) - Procedura koja dodaje
-//novi element s vrijednoscu x na zacelje reda.
-//DeQueueQ(Q) - Procedura koja brise
-//element s cela reda.
-//InitQ(Q) - Procedura koja inicira prazan red.
-//IsEmptyQ(Q) - logicka funkcija koja odgovara
-//na pitanje da li je red prazan.
-
-
